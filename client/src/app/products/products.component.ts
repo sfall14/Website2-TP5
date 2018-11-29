1a850b2d3db21294f225adee8f7f9c783587da58
sidy <sidy.fall@groupe-esigelec.org>
2018-11-29 18:50:47 -0500

    products page commencé

--- client/src/app/products/products.component.ts
+++ client/src/app/products/products.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ProductsService, Product } from '../products.service';
+
 
 /**
  * Defines the component responsible to manage the display of the products page.
@@ -9,4 +11,27 @@ import { Component } from '@angular/core';
 })
 export class ProductsComponent {
   // TODO: À compléter
+
+  products: Product[];
+  category: string;
+  sortingCriteria: string;
+
+  onSelectCategory(Selectedcategory: string): void {
+    if (!Selectedcategory) {
+      this.category = 'all';
+    } else {
+      this.category = Selectedcategory;
+    }
+  }
+
+  ngOnInit() {
+    this.productsService
+      .getProducts(null ,this.category)
+      .then(result => this.products = result)
+      .catch(error => console.log(error));
+  }
+
+  constructor(private productsService: ProductsService) { }
+
+  
 }
